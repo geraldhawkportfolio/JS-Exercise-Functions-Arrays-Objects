a1cd96e36e61389a014aee91ace512d7e1ec2e8a
Gerald Hawk <geraldrhawk@cookiecrumb.online>
2020-02-24 20:55:17 -0500

    retry tomorrow morning

--- index.js
+++ index.js
@@ -38,6 +38,25 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
  */
+
+var inventory = [
+	{ id: 1, car_make: 'Lincoln', car_model: 'Navigator', car_year: 2009 },
+	{ id: 2, car_make: 'Mazda', car_model: 'Miata MX-5', car_year: 2001 },
+	{ id: 3, car_make: 'Land Rover', car_model: 'Defender Ice Edition', car_year: 2010 },
+	{ id: 4, car_make: 'Honda', car_model: 'Accord', car_year: 1983 },
+	{ id: 5, car_make: 'Mitsubishi', car_model: 'Galant', car_year: 1990 },
+	{ id: 6, car_make: 'Honda', car_model: 'Accord', car_year: 1995 },
+	{ id: 7, car_make: 'Smart', car_model: 'Fortwo', car_year: 2009 },
+	{ id: 8, car_make: 'Audi', car_model: '4000CS Quattro', car_year: 1987 },
+	{ id: 9, car_make: 'Ford', car_model: 'Windstar', car_year: 1996 },
+	{ id: 10, car_make: 'Mercedes-Benz', car_model: 'E-Class', car_year: 2000 },
+	{ id: 11, car_make: 'Infiniti', car_model: 'G35', car_year: 2004 },
+	{ id: 12, car_make: 'Lotus', car_model: 'Esprit', car_year: 2004 },
+	{ id: 13, car_make: 'Chevrolet', car_model: 'Cavalier', car_year: 1997 },
+	{ id: 14, car_make: 'Dodge', car_model: 'Ram Van 1500', car_year: 1999 },
+	/// ... Truncated
+];
+
 function makePersonObject(id, name, email) {
 	const cred = {
 		id: 5,
@@ -191,13 +210,13 @@ function getLastCarInfo(inventory) {
  * For example, if getCarInfoById is invoked with the inventory and the number 1,
  * it will return `This is a Lincoln Navigator`.
  */
-function getCarInfoById(inventory, numyid) {
+function getCarInfoById(arr, num) {
 	/* code here */
-	var carinfo = inventory.find((item, index2) => {
-		return index2 === numyid;
-	});
-
-	return `This is a ${carinfo.car_make} ${carinfo.car_model}`;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i].id == num) {
+			return `This is a ${arr[i].car_make} ${arr[i].car_model}`;
+		}
+	}
 }
 
 /**
@@ -295,9 +314,9 @@ function getGermanCars(inventory) {
  *   return num * 2
  * }
  */
-//let sum = (a, b) => a + b; // code here!
-//let addFive = num => num + 5; // code here!
-//let argTimesTwo = num => num * 2; // code here!
+let sum = (a, b) => a + b; // code here!
+let addFive = num => num + 5; // code here!
+let argTimesTwo = num => num * 2; // code here!
 
 /**
  * ### Challenge `carMaker`
